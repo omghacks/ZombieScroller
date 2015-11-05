fb3db0f460fd1047bad32071870de6e29e1a2e0c
davihgtgx <missionbit@navajowhite-caption.local>
2015-11-04 17:08:43 -0800

    Jump function complete

--- game.js
+++ game.js
@@ -15,18 +15,21 @@ var mainState = {
     create: function () {
         // This function is called after the preload function
         // Here we set up the game, display sprites, etc.
-
-        // Create a game sprite from the logo image positioned
-        // at the center of the game world
+        game.physics.startSystem(Phaser.Physics.ARCADE);
+        game.physics.arcade.gravity.y = 200;
+        
 		var bg = game.add.image (0, 0,'background');
-        this.character = game.add.sprite(25,50, 'mainCharac');
-        this.character.scale.x = 0.5;
-        this.character.scale.y = 0.5;
+        this.character = game.add.sprite(400,50, 'mainCharac');
+        game.physics.enable(this.character, Phaser.Physics.ARCADE);
+        this.character.scale.x = 0.2;
+        this.character.scale.y = 0.2;
+        this.character.body.collideWorldBounds = true;
         // The position of the sprite should be based on the
         // center of the image (default is top-left)
         // Change background color to a gray color
         bg.width = game.width
         bg.height = game.height
+        this.jumpTimer = 0
 		
     },
     update: function () {
@@ -41,7 +44,15 @@ var mainState = {
             this.character.x -= 5;
         }
         
-        
+       
+
+
+        if (game.input.keyboard.isDown(Phaser.Keyboard.SPACEBAR) && this.character.body.onFloor() && game.time.now > this.jumpTimer)
+        {
+            this.character.body.velocity.y = -250;
+            this.jumpTimer = game.time.now + 750;
+        }
+       
         // Rotate the sprite by 1 degrees
 
     }
